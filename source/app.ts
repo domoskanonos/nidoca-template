5accb2df1b2d687528e463bcb2eef18daf867524
Domoskanonos <Tgg8855tt#>
2020-04-30 18:06:55 +0200

    init load

--- source/app.ts
+++ source/app.ts
@@ -5,8 +5,8 @@ import { AbstractApp } from '@domoskanonos/wc-atomic/source/abstract-app';
 import messageDE from './i18n/message-de.json';
 import messageEN from './i18n/message-en.json';
 
-import './page-dashboard';
-import './page-imprint';
+import './page/page-dashboard';
+import './page/page-imprint';
 
 @customElement('app-root')
 export class App extends AbstractApp {
